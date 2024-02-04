d562d1ffa1cbe8232f0937633ef4236a4909ce7b
admin <admin@premsan.com>
2024-02-05 02:10:33 +0800

    add package-info.java

---
+++ base/src/main/java/com/premsan/endless/base/package-info.java
@@ -0,0 +1,1 @@
+package com.premsan.endless.base;
--- wing/build.gradle.kts
+++ wing/build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 }
 
 dependencies {
+    implementation(project(":base"))
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
