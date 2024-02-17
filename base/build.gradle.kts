26a766459c3387ca1747380374a502e7127a82b4
admin <admin@premsan.com>
2024-02-17 23:27:12 +0800

    add jackson dependency

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -17,6 +17,8 @@ repositories {
 }
 
 dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
